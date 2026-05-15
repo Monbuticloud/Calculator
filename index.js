f9349c2bbd27913e2ea0e93a86d74baeec2c7b7b
Monbuticloud <hoangquocanhmon@icloud.com>
2026-05-15 22:05:03 +0700

    Refactor HTML and JavaScript for improved readability and consistency; update CSS for better styling

--- index.js
+++ index.js
@@ -1,62 +1,54 @@
 const display = document.getElementById("display");
-function appendToDisplay(input){
-    display.value += input;
+function appendToDisplay(input) {
+  display.value += input;
 }
 //｝
 function appendBrackets() {
-    display.value = ("("+display.value+")")
+  display.value = "(" + display.value + ")";
 }
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-function clearDisplay(){
-    display.value = '';
+function clearDisplay() {
+  display.value = "";
 }
-function root(base, number){
-    return Math.pow(number, (1 / base))
+function root(base, number) {
+  return Math.pow(number, 1 / base);
 }
-function cbrt(x){
-    return Math.pow(x, 1/3)
+function cbrt(x) {
+  return Math.pow(x, 1 / 3);
 }
 function sqrt(x) {
-    return Math.sqrt(x)
+  return Math.sqrt(x);
 }
 
-
-
-
-function ln(x){
-    return (Math.log(x))
+function ln(x) {
+  return Math.log(x);
 }
-function deleteTrailing(){
-    display.value  = display.value.slice(0, -1)
+function deleteTrailing() {
+  display.value = display.value.slice(0, -1);
 }
 
-async function calculate(){
-    try{
-                var x = display.value
-        var str = ''
-     for (let i = 0; i < x.length; i++) {
-         if (x[i] == '^') {
-             str = str + "**"
-         }
-         else{
-          str = str + x[i];
-      }}
-        if (str.length < 6){
-            
-        display.value = eval(str).toPrecision(6);
+async function calculate() {
+  try {
+    var x = display.value;
+    var str = "";
+    for (let i = 0; i < x.length; i++) {
+      if (x[i] == "^") {
+        str = str + "**";
+      } else {
+        str = str + x[i];
+      }
     }
-        else{
-            display.value = eval(str).toPrecision(str.length+1);
-        }
-    
+    if (str.length < 6) {
+      display.value = eval(str).toPrecision(6);
+    } else {
+      display.value = eval(str).toPrecision(str.length + 1);
     }
-    catch (error){
-        display.value = error;
+  } catch (error) {
+    display.value = error;
 
-        
-        await sleep(1500)
-        clearDisplay()
-    }
+    await sleep(1500);
+    clearDisplay();
+  }
 }
